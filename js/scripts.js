ea6c4ab62624ac165cf4e8fb8812cbf889356121
pavlichenko86 <vovan4ik86mail@gmail.com>
2017-12-19 16:42:00 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,24 @@ $(document).ready(function() {
     },
     loop: true
   });
+
+  var wow = new WOW({
+    boxClass: "wow1",
+    callback: function(box) {
+      $(".about-exp__count1").animateNumber({ number: 20 }, 2000);
+      $(".about-exp__count2").animateNumber({ number: 5580 }, 3000);
+      $(".about-exp__count3").animateNumber({ number: 760 }, 3000);
+    },
+    scrollContainer: null
+  });
+  wow.init();
+
+  var wow = new WOW({
+    boxClass: "wow2",
+    callback: function(box) {
+      $(".services-img").addClass("services-img-run");
+    },
+    scrollContainer: null
+  });
+  wow.init();
 });
